ae2eef0a249fe4567b4a80ba71f048773c7db868
yaditya298 <yaditya298@gmail.com>
2018-11-15 01:36:20 +0530

    Adds the required styling for active tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def i(text)
-    I18n.t(text)
+  def i(text, options={})
+    I18n.t(text, options)
+  end
+
+  def get_class(path = nil)
+    path && path == request.path ? 'list-group-item current-list' : 'list-group-item'
   end
 end
