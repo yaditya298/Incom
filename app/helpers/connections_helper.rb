966eb6a46cfc47ff4cb8b42e59b0852659c1b267
yaditya298 <yaditya298@gmail.com>
2018-11-15 12:34:52 +0530

    Makes the method movement for proper convention

--- app/helpers/connections_helper.rb
+++ app/helpers/connections_helper.rb
@@ -1,2 +1,6 @@
 module ConnectionsHelper
+  # Method to check if connection with given contact_id, group_id exists.
+  def get_checked_value(contact_id, group_id)
+    true if Connection.where(contact_id: contact_id, group_id: group_id).present?
+  end
 end
