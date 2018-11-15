ee81986227cba4505533b4f54b644446d0a1aa2c
yaditya298 <yaditya298@gmail.com>
2018-11-16 03:35:58 +0530

    This commit makes the following changes   * Removes unwanted code   * Adds method for connections check

--- app/helpers/connections_helper.rb
+++ app/helpers/connections_helper.rb
@@ -3,4 +3,8 @@ module ConnectionsHelper
   def get_checked_value(contact_id, group_id)
     true if Connection.where(contact_id: contact_id, group_id: group_id).present?
   end
+
+  def get_checked_property(group)
+    group.connections.size == current_user.contacts.count
+  end
 end
